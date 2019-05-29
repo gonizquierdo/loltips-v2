2f419468783e48d95d7edfa9dabbd51126f1f310
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-05-29 13:57:21 -0300

    More style updates. Concha de la pelota.

--- src/components/PremiumContent.js
+++ src/components/PremiumContent.js
@@ -6,12 +6,13 @@ export default class PremiumContent extends React.Component{
   render() {
 
     return (
-      <div className="container">
-        <h1 className="display-4 text-uppercase"> Not just data. <b>Insights</b> </h1>
-        <div style={{height: "200px"}}>
+      <div className="pt-4">
+        <div className="container bg-dark-transparent pb-4">
+          <h1 className="display-4 text-uppercase"> Not just data. <b>Insights</b> </h1>
+          <div style={{height: "200px"}}>
+          </div>
+          <Link to="/subscriptions-closed" className="btn btn-scattler btn-lg text-uppercase">Subscribe</Link>
         </div>
-        <Link to="/subscriptions-closed" className="btn btn-scattler btn-lg text-uppercase">Subscribe</Link>
-
       </div>
     )
   }
--- src/components/SubscriptionsClosed.js
+++ src/components/SubscriptionsClosed.js
@@ -7,15 +7,17 @@ export default class SubscriptionsClosed extends React.Component{
   render() {
 
     return (
-      <div className="container text-center bg-dark-transparent p-3 mt-3">
-        <h1 className="display-4"> We're <b>sorry</b>! </h1>
-        <img className="image-sorry rounded-circle bg-white" alt="Amumu is very sorry!" src={crying_amumu}/>
-        <p className="lead"> We have reached our <b>500</b> subscriptions limit for the alpha period. We will take new subscribers as soon as we can!</p>
+      <div className="pt-5">
+        <div className="container text-center bg-dark-transparent p-3">
+          <h1 className="display-4"> We're <b>sorry</b>! </h1>
+          <img className="image-sorry rounded-circle bg-white" alt="Amumu is very sorry!" src={crying_amumu}/>
+          <p className="lead"> We have reached our <b>500</b> subscriptions limit for the alpha period. We will take new subscribers as soon as we can!</p>
 
-        <div>
-          <h2 className="display-4"> Still interested? We have a <strong>waitlist</strong>! </h2>
-          <p className="lead"> Send us your information and we will contact you when we open new subscriptions.</p>
-          <WaitlistForm/>
+          <div>
+            <h2 className="display-4"> Still interested? We have a <strong>waitlist</strong>! </h2>
+            <p className="lead"> Send us your information and we will contact you when we open new subscriptions.</p>
+            <WaitlistForm/>
+          </div>
         </div>
       </div>
     )
