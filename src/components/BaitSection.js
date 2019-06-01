75d6c6836726e83a59d0c52f912edb7d1337af2d
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-05-31 21:03:10 -0300

    Content updates to bait section

--- src/components/BaitSection.js
+++ src/components/BaitSection.js
@@ -7,7 +7,22 @@ export default class BaitSection extends React.Component{
     return (
       <div className="container pt-3">
         <div className="bg-dark-transparent pb-3">
-          <h1 className="display-4">Did you find this information <strong> Useful</strong>?</h1>
+          <h1 className="display-4 text-uppercase">Was it <strong className="text-amumu"> Useful?</strong></h1>
+          <p className="lead"> We want you to keep up with what's important <b>without</b> having to go through multiple online sources</p>
+          <div className="container pt-3 pb-2">
+            <div className="row">
+              <div className="col">
+                <h2 className="text-uppercase"> Why we do this </h2>
+                <p className="text-justify lead">Beeing aware of everything that's happening in League of Legends can be a time consuming job. There's a lot of great information online but it's spread all over different sources. We want <b>you</b> to have access to the most important changes in the game in an easy way.</p>
+
+              </div>
+              <div className="col">
+                <h2 className="text-uppercase"> Our data sources </h2>
+                <p className="text-justify lead">At <b>LOLTIPS</b> we harvest data from a great number of public websites and Youtube channels. We keep up with Professional players and official information so we can bring the latest news to our feed.</p>
+
+              </div>
+            </div>
+          </div>
           <p> We want you to keep up with what's important <b>without</b> having to go through multiple online sources</p>
           <p className="lead">You can receive advise on your specific champions and league.</p>
           <p>Subscribe now and receive periodic updates to keep up with League of Legends.</p>
