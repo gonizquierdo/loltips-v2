5c513c7248d4c1979cff1343a5a54a2a4f3560a0
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-06-27 09:24:58 -0300

    Navbar progress

--- src/App.js
+++ src/App.js
@@ -35,28 +35,16 @@ function App() {
           </div>
         }
         />
-        <Route path="/loltips-v2" render={props =>
-          <div>
-            <MainPatchInfo patch={currentPatch}/>
-            <div className="container">
-              <div className="bg-dark-transparent">
-                <h1 className="display-4 text-uppercase font-weight-light">
-                  Research . Knowledge . <b className="text-amumu">Insights</b>
-                </h1>
-                <p className="lead text-justify p-3"> League of Legends is constantly evolving, and keeping up with it can be very time consuming. Our goal is to make it easy for you to take advantage of every buff and nerf, new strategies, builds, and everything that has changed around your favourite champions and current league.</p>
-                <h2 className="display-4 text-uppercase font-weight-light">
-                  Help us <b className="text-amumu">grow</b>
-                </h2>
-                <p className="lead text-center p-3"> We won't charge you for this information, but keep in mind we have awesome plans to develop all kinds of crazy stuff. Consider helping us to do it and get a special place in our hearts!</p>
-                <img alt="Btc qr code" className="pb-2" src={btc_qr}/>
-                <p className="text-monospace">38qjG4BS3Rqr66jbaZYm6rzjJGCDFuVrbz</p>
-              </div>
-            </div>
+        <Route path="/about-us" render={props =>
+          <div className="bg-dark-transparent">
+            <h1 className="display-4"> About Us </h1>
           </div>
-        }
-        />
+        }/>
 
-        <Route path="/about_us" component={() => <AboutUs />}/>
+        <Route path="/join-team" render={props =>
+          <div>
+          </div>
+        }/>
 
       </div>
     </Router>
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,36 +1,40 @@
 import React from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
-export default class CustomNavbar extends React.Component {
-  
+class CustomNavbar extends React.Component {
+
   getNavLinkClass = (path) => {
-          return this.props.location.pathname === path ? 'active' : '';
+          return this.props.location.pathname === path ? 'active' : '';}
 
   render() {
 
     return (
       <nav className="navbar-light navbar-expand-lg bg-light navbar navbar-fixed-top">
-        <Link className="text-dark font-weight-light navbar-brand logo" to="/">
+        <NavLink className="text-dark font-weight-light navbar-brand logo" to="/">
           SNEAKY<b className="text-amumu">BARON</b>
-        </Link>
+        </NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Patch highlights</a>
+            <li className={"nav-item " + this.getNavLinkClass("/")}>
+              <NavLink exact to="/" className="nav-link">Patch highlights</NavLink>
             </li>
-           <li className="nav-item">
-             <a className="nav-link" href="#">About us</a>
-           </li>
-           <li className="nav-item">
-             <a className="nav-link" href="#">Join the team</a>
-           </li>
+            <li className={"nav-item " + this.getNavLinkClass("/about-us")}>
+              <NavLink to="/about-us" className="nav-link">About us</NavLink>
+            </li>
+            <li className={"nav-item " + this.getNavLinkClass("/join-team")}>
+              <NavLink to="/join-team" className="nav-link">Join the team</NavLink>
+            </li>
           </ul>
         </div>
 
       </nav>
-)
+    )
  }
 }
+
+CustomNavbar = withRouter(CustomNavbar);
+export default CustomNavbar;
+
