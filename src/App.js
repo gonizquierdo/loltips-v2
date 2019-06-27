5fffd5494e6f79c77e0dfc1a7bbb0a124102b98e
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-06-27 07:33:36 -0300

    Navbar update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CustomNavbar from './components/CustomNavbar'
 import MainPatchInfo from './components/MainPatchInfo'
 import AboutUs from './components/AboutUs'
 import {getCurrentPatch} from './utils.js'
+import btc_qr from './assets/btc_qr.png'
 
 function App() {
   const currentPatch = getCurrentPatch();
@@ -18,11 +19,17 @@ function App() {
           <div>
             <MainPatchInfo patch={currentPatch}/>
             <div className="container">
-              <div className="bg-dark-transparent">
+              <div className="bg-dark-transparent pb-2">
                 <h1 className="display-4 text-uppercase font-weight-light">
                   Research . Knowledge . <b className="text-amumu">Insights</b>
                 </h1>
-                <p className="lead text-justify p-3"> League of Legends is constantly evolving, and keeping up with it can be very time consuming. Our goal is to make it easy for you to take advantage of every buff and nerf, new strategies, builds, and everything that has changed around your favourite champions and current league.</p>
+                <p className="lead text-center p-3"> League of Legends is constantly evolving, and keeping up with it can be very time consuming. Our goal is to make it easy for you to take advantage of every buff and nerf, new strategies, builds, and everything that has changed around your favourite champions and current league.</p>
+                <h2 className="display-4 text-uppercase font-weight-light">
+                  Help us <b className="text-amumu">grow</b>
+                </h2>
+                <p className="lead text-center p-3"> We won't charge you for this information, but keep in mind we have awesome plans to develop all kinds of crazy stuff. Consider helping us to do it and get a special place in our hearts!</p>
+                <img alt="Btc qr code" className="pb-2" src={btc_qr}/>
+                <p className="text-monospace">38qjG4BS3Rqr66jbaZYm6rzjJGCDFuVrbz</p>
               </div>
             </div>
           </div>
@@ -37,6 +44,12 @@ function App() {
                   Research . Knowledge . <b className="text-amumu">Insights</b>
                 </h1>
                 <p className="lead text-justify p-3"> League of Legends is constantly evolving, and keeping up with it can be very time consuming. Our goal is to make it easy for you to take advantage of every buff and nerf, new strategies, builds, and everything that has changed around your favourite champions and current league.</p>
+                <h2 className="display-4 text-uppercase font-weight-light">
+                  Help us <b className="text-amumu">grow</b>
+                </h2>
+                <p className="lead text-center p-3"> We won't charge you for this information, but keep in mind we have awesome plans to develop all kinds of crazy stuff. Consider helping us to do it and get a special place in our hearts!</p>
+                <img alt="Btc qr code" className="pb-2" src={btc_qr}/>
+                <p className="text-monospace">38qjG4BS3Rqr66jbaZYm6rzjJGCDFuVrbz</p>
               </div>
             </div>
           </div>
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,15 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+
 export default class CustomNavbar extends React.Component {
+  
+  getNavLinkClass = (path) => {
+          return this.props.location.pathname === path ? 'active' : '';
 
   render() {
 
     return (
-      <div className="bg-light nav navbar navbar-fixed-top">
-        <Link className="text-dark lead" to="/">
-          SNEAKYBARON.GG LOGO
+      <nav className="navbar-light navbar-expand-lg bg-light navbar navbar-fixed-top">
+        <Link className="text-dark font-weight-light navbar-brand logo" to="/">
+          SNEAKY<b className="text-amumu">BARON</b>
         </Link>
-      </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Patch highlights</a>
+            </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">About us</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Join the team</a>
+           </li>
+          </ul>
+        </div>
+
+      </nav>
 )
  }
 }
