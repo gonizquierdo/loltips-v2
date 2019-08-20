87669a99bc4b81f7d638f9e32f7adb6b51cac6b2
Gonzalo Izquierdo Brown <ibgonzalo@gmail.com>
2019-08-20 11:15:07 -0300

    Files from S3

--- src/components/TeamfightTactics.js
+++ src/components/TeamfightTactics.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import axios from 'axios'
+
 import { t, locale } from '../i18n.js';
 import { getPatchData } from '../utils.js';
 import icon from '../assets/TTF_Items.png';
-import { Link, Route, Switch } from 'react-router-dom';
-
 
 export default class TeamfightTactics extends React.Component {
   props: {
@@ -19,10 +19,15 @@ export default class TeamfightTactics extends React.Component {
   }
 
   setView(view){
-    console.log(view)
     this.setState({showing: view})
   }
-
+  componentDidMount() {
+    axios.get('https://sneaky-static-data.s3.us-east-2.amazonaws.com/parches_new/'+locale()+'/'+this.props.patch.split(".").join("")+'.json')
+    .then(response => {
+      let json = response.data.tft
+      this.setState({ patch_data: json })
+    })
+  }
   get_link_for_language() {
       const lang = locale();
       if (lang === 'en') {
@@ -33,49 +38,59 @@ export default class TeamfightTactics extends React.Component {
       }
   }
 
-  render_notes_component() {
-      const link = this.get_link_for_language()
-      const { patch } = this.props
-      const patch_data = getPatchData("915");
-      return (
-          <div className="info-panel">
-              <h2 className="border-blue-3">Patch Notes</h2>
-              <div>
-                  <p>{t('patch_notes.summary')}</p>
-              </div>
-
-              <div>
-                  <h4> {t('patch_notes.tactical_buffs')}</h4>
-                  <ul>
-                      {
-                          patch_data.tft.buffs.map((buff, index) =>
-                              <li key={index}>{buff}</li>
-                          )
-                      }
-                  </ul>
-                  <h4> {t('patch_notes.strategic_nerfs')}</h4>
-                  <ul>
-                      {
-                          patch_data.tft.nerfs.map((nerf, index) =>
-                              <li key={index}>{nerf}</li>
-                          )
-                      }
-                  </ul>
-                  <h4> {t('patch_notes.tft_updates')}</h4>
-                  <ul>
-                      {
-                          patch_data.tft.updates.map((update, index) =>
-                              <li key={index}>{update}</li>
-                          )
-                      }
-                  </ul>
-              </div>
-              <div className="text-center w-100">
-                  <a href={link} target="_new">
-                      <button type="button" className="btn btn-scattler">{t('patch_notes.official_notes')}</button>
-                  </a>
-              </div>
+  render_notes_text(patch_data) {
+    const link = this.get_link_for_language()
+    return (
+      <div className="info-panel">
+          <h2 className="border-blue-3">Patch Notes</h2>
+          <div>
+              <p>{t('patch_notes.summary')}</p>
           </div>
+
+          <div>
+              <h4> {t('patch_notes.tactical_buffs')}</h4>
+              <ul>
+                  {
+                      patch_data.buffs.map((buff, index) =>
+                          <li key={index}>{buff}</li>
+                      )
+                  }
+              </ul>
+              <h4> {t('patch_notes.strategic_nerfs')}</h4>
+              <ul>
+                  {
+                      patch_data.nerfs.map((nerf, index) =>
+                          <li key={index}>{nerf}</li>
+                      )
+                  }
+              </ul>
+              <h4> {t('patch_notes.tft_updates')}</h4>
+              <ul>
+                  {
+                      patch_data.updates.map((update, index) =>
+                          <li key={index}>{update}</li>
+                      )
+                  }
+              </ul>
+          </div>
+          <div className="text-center w-100">
+              <a href={link} target="_new">
+                  <button type="button" className="btn btn-scattler">{t('patch_notes.official_notes')}</button>
+              </a>
+          </div>
+      </div>
+    )
+  }
+
+  render_notes_component() {
+      const { patch } = this.props
+      const { patch_data } = this.state
+      return (
+
+        Object.keys(patch_data).length ? this.render_notes_text(patch_data) : (<div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+        )
       )
   }
 
